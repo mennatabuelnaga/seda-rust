f6b5a15f7e3f38b63cc85188d0e2abca258a03f8
gluax <jonathan.t.pavlik@gmail.com>
2022-10-17 14:31:22 +0200

    feat: simple cli and parsing

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "sedad")]
+#[command(author = "https://github.com/SedaProtocol")]
+#[command(version = "0.0.1")]
+#[command(about = "For interacting with the seda protocol.", long_about = None)]
+struct Options {}
+
 fn main() {
-    println!("Hello, world!");
+    let _options = Options::parse();
 }
