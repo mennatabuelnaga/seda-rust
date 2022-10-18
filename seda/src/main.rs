6446af60761a7e0106a4767a00e2bd2b422335e9
gluax <jonathan.t.pavlik@gmail.com>
2022-10-18 10:37:05 +0200

    feat: setup subcommands for future implementations

--- seda/src/main.rs
+++ seda/src/main.rs
@@ -1,11 +1,20 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(name = "seda")]
 #[command(author = "https://github.com/SedaProtocol")]
 #[command(version = "0.0.1")]
 #[command(about = "For interacting with the seda protocol.", long_about = None)]
-struct Options {}
+struct Options {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Sends a JsonRPC message to the node's server.
+    Register,
+}
 
 fn main() {
     let _options = Options::parse();
