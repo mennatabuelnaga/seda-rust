733f5e0eb8ec1321b0fb1f77d9e5f6851eb30b45
gluax <jonathan.t.pavlik@gmail.com>
2022-10-18 10:46:51 +0200

    chore: change seda version to match version in the cargo file

--- seda/src/main.rs
+++ seda/src/main.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(name = "seda")]
 #[command(author = "https://github.com/SedaProtocol")]
-#[command(version = "0.0.1")]
+#[command(version = "0.1.0")]
 #[command(about = "For interacting with the seda protocol.", long_about = None)]
 struct Options {
     #[command(subcommand)]
