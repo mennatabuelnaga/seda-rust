12b65369406a9e2c0a5619ed4876fa79871598f9
Jameson Hodge <contact@jamesonhodge.com>
2022-10-19 17:26:16 -0400

    feat(contracts): implement storage refund

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -37,12 +37,13 @@ impl MainchainContract {
         self.nodes.get(&node_id).expect("Node does not exist")
     }
 
-    pub fn assert_permission(&self, account_id: &AccountId, correct_account_id: &AccountId) {
-        assert_eq!(
-            account_id, correct_account_id,
-            "Only {} can call this method",
-            correct_account_id
-        );
+    pub fn assert_node_owner(&self, account_id: &AccountId, node_owner: &AccountId) {
+        assert_eq!(account_id, node_owner, "Only {} can call this method", node_owner);
+    }
+
+    pub fn assert_node_pending_owner(&self, account_id: &AccountId, node_pending_owner: &Option<AccountId>) {
+        let pending_owner = node_pending_owner.as_ref().expect("Node does not have a pending owner");
+        assert_eq!(account_id, pending_owner, "Only {} can call this method", pending_owner);
     }
 }
 
@@ -86,12 +87,30 @@ impl MainchainContract {
         );
     }
 
-    /// Update the pending owner of a node
+    /// Removes a node and refunds storage deposit
+    pub fn remove_node(&mut self, node_id: U64) {
+        // keep track of storage usage
+        let initial_storage_usage = env::storage_usage();
+
+        let account_id = env::signer_account_id();
+        let node = self.get_expect_node(node_id.into());
+
+        self.assert_node_owner(&account_id, &node.owner);
+
+        log!("{} removed node_id {}", account_id, u64::from(node_id));
+        self.nodes.remove(&node_id.into());
+
+        // refund storage deposit
+        let storage_cost = env::storage_byte_cost() * u128::from(initial_storage_usage - env::storage_usage());
+        log!("Refunding {} for storage deposit to {}", storage_cost, account_id);
+    }
+
+    /// Updates the pending owner of a node
     pub fn set_node_pending_owner(&mut self, node_id: U64, new_owner: String) {
         let account_id = env::signer_account_id();
         let mut node = self.get_expect_node(node_id.into());
 
-        self.assert_permission(&account_id, &node.owner);
+        self.assert_node_owner(&account_id, &node.owner);
 
         log!(
             "{} updated node_id {} pending_owner to {}",
@@ -103,12 +122,12 @@ impl MainchainContract {
         self.nodes.insert(&u64::from(node_id), &node);
     }
 
-    /// Finalize the pending owner change
+    /// Finalizes the pending owner change
     pub fn become_node_owner(&mut self, node_id: U64) {
         let account_id = env::signer_account_id();
         let mut node = self.get_expect_node(node_id.into());
 
-        self.assert_permission(&account_id.clone(), &node.pending_owner.unwrap());
+        self.assert_node_pending_owner(&account_id.clone(), &node.pending_owner);
 
         log!("{} became owner of node_id {}", account_id, u64::from(node_id),);
         node.owner = account_id;
@@ -120,7 +139,7 @@ impl MainchainContract {
         let account_id = env::signer_account_id();
         let mut node = self.get_expect_node(node_id.into());
 
-        self.assert_permission(&account_id, &node.owner);
+        self.assert_node_owner(&account_id, &node.owner);
 
         log!(
             "{} updated node with id {} ip address to {}",
@@ -136,7 +155,7 @@ impl MainchainContract {
         let account_id = env::signer_account_id();
         let mut node = self.get_expect_node(node_id.into());
 
-        self.assert_permission(&account_id, &node.owner);
+        self.assert_node_owner(&account_id, &node.owner);
 
         log!(
             "{} updated node with id {} port to {}",
@@ -146,18 +165,6 @@ impl MainchainContract {
         );
         node.port = new_port.into();
         self.nodes.insert(&node_id.into(), &node);
-    }
-
-    pub fn remove_node(&mut self, node_id: U64) {
-        let account_id = env::signer_account_id();
-        let node = self.get_expect_node(node_id.into());
-
-        self.assert_permission(&account_id, &node.owner);
-
-        log!("{} removed node with id {}", account_id, u64::from(node_id));
-        self.nodes.remove(&node_id.into());
-
-        // TODO: refund storage deposit
     }
 
     pub fn get_node_owner(&self, node_id: U64) -> Option<AccountId> {
@@ -204,26 +211,34 @@ mod tests {
 
     use super::*;
 
-    fn get_context(is_view: bool, signer_account_id: String) -> VMContext {
+    fn get_context_view() -> VMContext {
+        VMContextBuilder::new().is_view(true).build()
+    }
+    fn get_context(signer_account_id: String) -> VMContext {
         VMContextBuilder::new()
             .signer_account_id(signer_account_id.parse().unwrap())
-            .is_view(is_view)
+            .is_view(false)
+            .build()
+    }
+    fn get_context_with_deposit(signer_account_id: String) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id(signer_account_id.parse().unwrap())
+            .is_view(false)
             .attached_deposit(810_000_000_000_000_000_000) // required for register_node()
             .build()
     }
 
     #[test]
     fn test_register_and_get_node() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
         assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
 
         // check owner, ip_address, and port
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(
             "bob_near".to_string(),
             contract.get_node_owner(U64(1)).unwrap().to_string()
@@ -241,57 +256,68 @@ mod tests {
     }
 
     #[test]
-    fn test_remove_node() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
+    #[should_panic(expected = "Insufficient storage, need 810000000000000000000")]
+    fn test_register_not_enough_storage() {
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
-        assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
+    }
+
+    #[test]
+    fn test_remove_node() {
+        let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
+        contract.register_node("0.0.0.0".to_string(), U64(8080));
 
         // check the ip address after registering
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(Some("0.0.0.0".to_string()), contract.get_node_ip_address(U64(1)));
 
         // remove the node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context("bob_near".to_string()));
         contract.remove_node(U64(1));
+        assert_eq!(
+            get_logs(),
+            vec![
+                "bob_near removed node_id 1",
+                "Refunding 810000000000000000000 for storage deposit to bob_near"
+            ]
+        );
 
         // check the ip address after removing
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(None, contract.get_node_ip_address(U64(1)));
     }
 
     #[test]
     #[should_panic(expected = "Only bob_near can call this method")]
     fn test_remove_node_wrong_owner() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
         assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
 
         // check the ip address after registering
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(Some("0.0.0.0".to_string()), contract.get_node_ip_address(U64(1)));
 
         // try removing the node
-        let context = get_context(false, "alice_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context("alice_near".to_string()));
         contract.remove_node(U64(1));
     }
 
     #[test]
     fn test_set_node_ip_address_and_port() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
         assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
 
@@ -300,8 +326,7 @@ mod tests {
         contract.set_node_port(U64(1), U64(8081));
 
         // check the ip address and port after updating
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!("1.1.1.1".to_string(), contract.get_node_ip_address(U64(1)).unwrap());
         assert_eq!(U64(8081), contract.get_node_port(U64(1)).unwrap());
         assert_eq!(
@@ -312,10 +337,10 @@ mod tests {
 
     #[test]
     fn test_new_owner() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
         assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
 
@@ -323,21 +348,18 @@ mod tests {
         contract.set_node_pending_owner(U64(1), "alice_near".to_string());
 
         // check pending owner
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(
             "alice_near".to_string(),
             contract.get_node_pending_owner(U64(1)).unwrap().to_string()
         );
 
         // accept ownership
-        let context = get_context(false, "alice_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context("alice_near".to_string()));
         contract.become_node_owner(U64(1));
 
         // check owner
-        let context = get_context(true, "alice_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(
             "alice_near".to_string(),
             contract.get_node_owner(U64(1)).unwrap().to_string()
@@ -345,12 +367,27 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "Node does not have a pending owner")]
+    fn test_wrong_owner_simple() {
+        let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
+        contract.register_node("0.0.0.0".to_string(), U64(8080));
+        assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
+
+        // accept ownership from wrong owner
+        testing_env!(get_context("alice_near".to_string()));
+        contract.become_node_owner(U64(1));
+    }
+
+    #[test]
     #[should_panic(expected = "Only alice_near can call this method")]
     fn test_wrong_owner() {
-        // register node
-        let context = get_context(false, "bob_near".to_string());
-        testing_env!(context);
         let mut contract = MainchainContract::new();
+
+        // register node
+        testing_env!(get_context_with_deposit("bob_near".to_string()));
         contract.register_node("0.0.0.0".to_string(), U64(8080));
         assert_eq!(get_logs(), vec!["bob_near registered node_id 1"]);
 
@@ -358,24 +395,21 @@ mod tests {
         contract.set_node_pending_owner(U64(1), "alice_near".to_string());
 
         // check pending owner
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context_view());
         assert_eq!(
             "alice_near".to_string(),
             contract.get_node_pending_owner(U64(1)).unwrap().to_string()
         );
 
         // accept ownership from wrong owner
-        let context = get_context(false, "franklin_near".to_string());
-        testing_env!(context);
+        testing_env!(get_context("franklin_near".to_string()));
         contract.become_node_owner(U64(1));
     }
 
     #[test]
     fn get_nonexistent_message() {
-        let context = get_context(true, "bob_near".to_string());
-        testing_env!(context);
         let contract = MainchainContract::new();
+        testing_env!(get_context_view());
         assert_eq!(None, contract.get_node_ip_address(U64(1)));
         assert_eq!(get_logs(), vec!["get_node_ip_address for node_id 1"])
     }
