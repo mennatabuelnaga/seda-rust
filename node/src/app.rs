73f37c322d5d1e02d68afd59019376d44b90adcc
Mario Cao <mariocao@gmail.com>
2022-10-24 11:36:05 +0000

    chore(node): add some start/stop traces

--- node/src/app.rs
+++ node/src/app.rs
@@ -6,4 +6,21 @@ pub struct App;
 impl Actor for App {
     type Context = Context<App>;
 
+    fn started(&mut self, _ctx: &mut Self::Context) {
+        println!("Node starting...");
+        let banner = r#"
+         _____ __________  ___         ____  __  _____________
+        / ___// ____/ __ \/   |       / __ \/ / / / ___/_  __/
+        \__ \/ __/ / / / / /| |______/ /_/ / / / /\__ \ / /
+       ___/ / /___/ /_/ / ___ /_____/ _, _/ /_/ /___/ // /
+      /____/_____/_____/_/  |_|    /_/ |_|\____//____//_/
+        "#;
+        println!("{}", banner);
+
+        // Node starting logic...
+    }
+
+    fn stopped(&mut self, _ctx: &mut Self::Context) {
+        println!("Node stopped");
+    }
 }
