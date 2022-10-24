500c6b83c842f38308f396d372e7ebf97d3f5aaa
gluax <16431709+gluax@users.noreply.github.com>
2022-10-24 10:40:59 -0700

    chore(runtime): push all changes not just runtime core

--- runtime/src/adapters.rs
+++
@@ -1,8 +0,0 @@
-pub trait DatabaseAdapter {
-    fn set(&mut self, key: &str, value: &str);
-    fn get(&self, key: &str);
-}
-
-pub struct Adapters {
-    pub database: Box<dyn DatabaseAdapter + Send>,
-}
