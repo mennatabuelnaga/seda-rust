e3759e1507c9c188c993e5713d9e0ff9389f5417
jasperdg <jasperdegooijer@gmail.com>
2022-11-08 15:20:31 +0000

    refactor(adapters): MainChainAdapter should be ready to test, after cli is modified to new adapter format

--- adapters/src/mainchain_adapter.rs
+++ adapters/src/mainchain_adapter.rs
@@ -1,5 +1,3 @@
-// use core::num::flt2dec::Sign;
-
 use async_trait;
 use jsonrpsee_types::Params;
 use near_crypto::InMemorySigner;
@@ -32,7 +30,7 @@ pub struct MainChainAdapter {
 
 #[async_trait::async_trait]
 pub trait MainChainAdapterTrait {
-    fn init(&mut self, rpc_endpoint: String);
+    fn new(&mut self, rpc_endpoint: String) -> Self;
     async fn sign_and_send_tx(&self, tx_params: TransactionParams) -> Result<FinalExecutionStatus>;
     async fn sign_tx(&self, tx_params: TransactionParams) -> Result<SignedTransaction>;
     async fn send_tx(&self, signed_tx: SignedTransaction) -> Result<FinalExecutionStatus>;
@@ -41,15 +39,14 @@ pub trait MainChainAdapterTrait {
     async fn get_node_owner(&self, params: Params<'_>) -> Result<String>;
     async fn get_node_socket_address(&self, params: Params<'_>) -> Result<String>;
     async fn get_nodes(&self, params: Params<'_>) -> Result<String>;
-    async fn register_node(&self, params: Params<'_>) -> Result<FinalExecutionStatus>;
-    async fn remove_node(&self, params: Params<'_>) -> Result<FinalExecutionStatus>;
-    async fn set_node_socket_address(&self, params: Params<'_>) -> Result<FinalExecutionStatus>;
 }
 
 #[async_trait::async_trait]
 impl MainChainAdapterTrait for MainChainAdapter {
-    fn init(&mut self, rpc_endpoint: String) {
-        self.client = JsonRpcClient::connect(rpc_endpoint);
+    fn new(&mut self, rpc_endpoint: String) -> Self {
+        Self {
+            client: JsonRpcClient::connect(rpc_endpoint),
+        }
     }
 
     async fn sign_and_send_tx(&self, tx_params: TransactionParams) -> Result<FinalExecutionStatus> {
@@ -61,7 +58,7 @@ impl MainChainAdapterTrait for MainChainAdapter {
         let signer_account_id: AccountId = tx_params.signer_acc_str.parse()?;
 
         let signer_secret_key: near_crypto::SecretKey = tx_params.signer_sk_str.parse()?;
-        let signer = near_crypto::InMemorySigner::from_secret_key(signer_account_id, signer_secret_key);
+        let signer = InMemorySigner::from_secret_key(signer_account_id, signer_secret_key);
         let access_key_query_response = self
             .client
             .call(methods::query::RpcQueryRequest {
@@ -159,133 +156,53 @@ impl MainChainAdapterTrait for MainChainAdapter {
     }
 
     async fn get_node_owner(&self, params: Params<'_>) -> Result<String> {
-        Err(MainChainAdapterError::CallViewMethod)
+        let method_name = "get_node_owner".to_string();
+        let mut seq = params.sequence();
+
+        let contract_id: String = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("contract_id".to_string()))?;
+        let node_id: Number = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("node_id".to_string()))?;
+
+        let args = json!({"node_id":node_id.to_string()}).to_string().into_bytes();
+
+        self.view(contract_id, method_name, args).await
     }
 
     async fn get_node_socket_address(&self, params: Params<'_>) -> Result<String> {
-        Err(MainChainAdapterError::CallViewMethod)
+        let method_name = "get_node_socket_address".to_string();
+        let mut seq = params.sequence();
+        let contract_id: String = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("contract_id".to_string()))?;
+        let node_id: Number = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("node_id".to_string()))?;
+
+        let args = json!({"node_id":node_id.to_string()}).to_string().into_bytes();
+
+        self.view(contract_id, method_name, args).await
     }
 
     async fn get_nodes(&self, params: Params<'_>) -> Result<String> {
-        Err(MainChainAdapterError::CallViewMethod)
-    }
+        let method_name = "get_nodes".to_string();
+        let mut seq = params.sequence();
+        let contract_id: String = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("contract_id".to_string()))?;
+        let limit: Number = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("limit".to_string()))?;
+        let offset: Number = seq
+            .next()
+            .map_err(|_| MainChainAdapterError::MissingParam("offset".to_string()))?;
 
-    async fn register_node(&self, params: Params<'_>) -> Result<FinalExecutionStatus> {
-        Err(MainChainAdapterError::CallViewMethod)
-    }
+        let args = json!({"limit": limit.to_string(), "offset":offset.to_string()})
+            .to_string()
+            .into_bytes();
 
-    async fn remove_node(&self, params: Params<'_>) -> Result<FinalExecutionStatus> {
-        Err(MainChainAdapterError::CallViewMethod)
-    }
-
-    async fn set_node_socket_address(&self, params: Params<'_>) -> Result<FinalExecutionStatus> {
-        Err(MainChainAdapterError::CallViewMethod)
+        self.view(contract_id, method_name, args).await
     }
 }
-
-// pub async fn get_node_owner(params: Params<'_>) -> Result<String> {
-//     let method_name = "get_node_owner".to_string();
-//     let mut seq = params.sequence();
-
-//     let contract_id: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("contract_id".to_string()))?;     let
-// node_id: Number = seq         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("node_id".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     let args = json!({"node_id":
-// node_id.to_string()}).to_string().into_bytes();
-//     call_view_method(contract_id, method_name, args, server_addr).await
-// }
-
-// pub async fn get_node_socket_address(params: Params<'_>) -> Result<String> {
-//     let method_name = "get_node_socket_address".to_string();
-//     let mut seq = params.sequence();
-//     let contract_id: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("contract_id".to_string()))?;     let
-// node_id: Number = seq         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("node_id".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     let args = json!({"node_id":
-// node_id.to_string()}).to_string().into_bytes();
-//     call_view_method(contract_id, method_name, args, server_addr).await
-// }
-
-// pub async fn get_nodes(params: Params<'_>) -> Result<String> {
-//     let method_name = "get_nodes".to_string();
-//     let mut seq = params.sequence();
-//     let contract_id: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("contract_id".to_string()))?;     let
-// limit: Number = seq         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("limit".to_string()))?;     let offset:
-// Number = seq         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("offset".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     let args = json!({"limit": limit.to_string(), "offset":
-// offset.to_string()})         .to_string()
-//         .into_bytes();
-//     call_view_method(contract_id, method_name, args, server_addr).await
-// }
-
-// pub async fn register_node(params: Params<'_>) ->
-// Result<FinalExecutionStatus> {     let mut seq = params.sequence();
-//     let signed_tx: SignedTransaction = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("signed_tx".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     call_change_method(signed_tx, server_addr).await
-// }
-
-// pub async fn remove_node(params: Params<'_>) -> Result<FinalExecutionStatus>
-// {     let mut seq = params.sequence();
-//     let signed_tx: SignedTransaction = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("signed_tx".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     call_change_method(signed_tx, server_addr).await
-// }
-
-// pub async fn set_node_socket_address(params: Params<'_>) ->
-// Result<FinalExecutionStatus> {     let mut seq = params.sequence();
-//     let signed_tx: SignedTransaction = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("signed_tx".to_string()))?;
-//     let server_addr: String = seq
-//         .next()
-//         .map_err(|_|
-// MainChainAdapterError::MissingParam("server_addr".to_string()))?;
-
-//     call_change_method(signed_tx, server_addr).await
-// }
