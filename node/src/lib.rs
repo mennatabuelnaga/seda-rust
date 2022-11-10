41e7c9712494191bbb97a986ebd2922e53106e9b
Mario Cao <mariocao@gmail.com>
2022-11-10 10:20:48 +0000

    feat(node, cli): add rpc and p2p params

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,4 +1,5 @@
 mod app;
+mod errors;
 mod p2p;
 mod rpc;
 use actix::prelude::*;
@@ -7,14 +8,21 @@ use rpc::JsonRpcServer;
 
 use crate::{app::Shutdown, p2p::p2p_listen, rpc::Stop};
 
-pub fn run(peer_address: Option<String>) {
-    let system = System::new();
+pub fn run(
+    jsonrpc_server_address: Option<String>,
+    p2p_server_address: Option<String>,
+    known_peers: Option<Vec<String>>,
+) {
+    // TODO: add config (from CLI, config files and secrets from ENV)
 
     // Initialize actors inside system context
+    let system = System::new();
     system.block_on(async {
-        p2p_listen(peer_address).await.unwrap();
+        // Node application
         let app = App.start();
-        let rpc_server = JsonRpcServer::build()
+
+        // Json-RPC Server
+        let rpc_server = JsonRpcServer::build(&jsonrpc_server_address.unwrap_or_else(|| "127.0.0.1:12345".to_string()))
             .await
             .expect("Error starting jsonrpsee server")
             .start();
