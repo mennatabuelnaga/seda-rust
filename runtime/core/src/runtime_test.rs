580381671a52d20ca0e72d5432e4ea21a4ddb82e
mnaga <mennat.abuelnaga@gmail.com>
2022-12-06 15:22:43 +0200

    fix: http_fetch_test_success + test_promise_queue_http_fetch test

--- runtime/core/src/runtime_test.rs
+++ runtime/core/src/runtime_test.rs
@@ -210,8 +210,8 @@ async fn test_cli_demo_view_chain() {
 //     let runtime_execution_result = runtime
 //         .start_runtime::<RuntimeTestAdapter>(
 //             VmConfig {
-//                 args:         vec!["view".to_string(), chain, contract_id,
-// method_name, args],                 program_name: "consensus".to_string(),
+//                 args:         vec!["view".to_string(), chain, contract_id, method_name, args],
+//                 program_name: "consensus".to_string(),
 //                 start_func:   None,
 //                 debug:        true,
 //             },
@@ -220,9 +220,8 @@ async fn test_cli_demo_view_chain() {
 //         .await;
 //     assert!(runtime_execution_result.is_ok());
 
-//     let db_result =
-// RuntimeTestAdapter::db_get("chain_view_result").await.unwrap();     assert!
-// (db_result.is_some());
+//     let db_result = RuntimeTestAdapter::db_get("chain_view_result").await.unwrap();
+//     assert!(db_result.is_some());
 
 //     assert_eq!(db_result.unwrap(), "From another mainchain".to_string());
 // }
@@ -300,7 +299,8 @@ async fn test_cli_demo_view_chain() {
 //     let method = "register_node";
 //     let socket_address = "0.0.0.0:8080";
 //     let args = json!({"socket_address":
-// socket_address.to_string()}).to_string();
+// socket_address.to_string()})
+//     .to_string();
 
 //     let runtime_execution_result = runtime
 //         .start_runtime::<RuntimeTestAdapter>(
@@ -322,7 +322,7 @@ async fn test_cli_demo_view_chain() {
 
 //     assert!(runtime_execution_result.is_ok());
 
-//     let db_result =
-// RuntimeTestAdapter::db_get("chain_call_result").await.unwrap();     assert!
-// (db_result.is_some());     assert_eq!(db_result.unwrap(), "Called change From
-// another chain".to_string()); }
+//     let db_result = RuntimeTestAdapter::db_get("chain_call_result").await.unwrap();
+//     assert!(db_result.is_some());
+//     assert_eq!(db_result.unwrap(), "Called change From another chain".to_string());
+// }
