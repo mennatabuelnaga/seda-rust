de9f15a396937e08383547522c50a819ce7a433c
mnaga <mennat.abuelnaga@gmail.com>
2023-01-12 13:55:48 +0200

    feat: decrease account unstaked balance in the initial withdraw() instead of the callback

--- contracts/mainchain/src/account.rs
+++ contracts/mainchain/src/account.rs
@@ -41,7 +41,7 @@ pub struct HumanReadableAccount {
     pub can_withdraw:     bool,
 }
 
-/// Contract internal methods
+/// Contract account private methods
 impl MainchainContract {
     pub fn internal_get_account(&self, account_id: &AccountId) -> Account {
         self.accounts.get(account_id).unwrap_or_default()
@@ -53,69 +53,5 @@ impl MainchainContract {
         } else {
             self.accounts.remove(account_id);
         }
-    }
-
-    /*************** */
-    /* View methods */
-    /*************** */
-
-    /// Returns the unstaked balance of the given account.
-    pub fn get_account_unstaked_balance(&self, account_id: AccountId) -> U128 {
-        self.get_human_readable_account(account_id).unstaked_balance
-    }
-
-    /// Returns the staked balance of the given account.
-    /// NOTE: This is computed from the amount of "stake" shares the given
-    /// account has and the current amount of total staked balance and total
-    /// stake shares on the account.
-    pub fn get_account_staked_balance(&self, account_id: AccountId) -> U128 {
-        self.get_human_readable_account(account_id).staked_balance
-    }
-
-    /// Returns the total balance of the given account (including staked and
-    /// unstaked balances).
-    pub fn get_account_total_balance(&self, account_id: AccountId) -> U128 {
-        let account = self.get_human_readable_account(account_id);
-        (account.unstaked_balance.0 + account.staked_balance.0).into()
-    }
-
-    /// Returns `true` if the given account can withdraw tokens in the current
-    /// epoch.
-    pub fn is_account_unstaked_balance_available(&self, account_id: AccountId) -> bool {
-        self.get_human_readable_account(account_id).can_withdraw
-    }
-
-    /// Returns the total staking balance.
-    pub fn get_total_staked_balance(&self) -> U128 {
-        self.total_staked_balance.into()
-    }
-
-    /// Returns human readable representation of the account for the given
-    /// account ID.
-    pub fn get_human_readable_account(&self, account_id: AccountId) -> HumanReadableAccount {
-        let account = self.internal_get_account(&account_id);
-        HumanReadableAccount {
-            account_id,
-            unstaked_balance: account.unstaked.into(),
-            staked_balance: self
-                .staked_amount_from_num_shares_rounded_down(account.stake_shares)
-                .into(),
-            can_withdraw: account.unstaked_available_epoch_height <= env::epoch_height(),
-        }
-    }
-
-    /// Returns the number of accounts that have positive balance on this
-    /// staking pool.
-    pub fn get_number_of_accounts(&self) -> u64 {
-        self.accounts.len()
-    }
-
-    /// Returns the list of accounts
-    pub fn get_human_readable_accounts(&self, from_index: u64, limit: u64) -> Vec<HumanReadableAccount> {
-        let keys = self.accounts.keys_as_vector();
-
-        (from_index..std::cmp::min(from_index + limit, keys.len()))
-            .map(|index| self.get_human_readable_account(keys.get(index).unwrap()))
-            .collect()
     }
 }
--- contracts/mainchain/src/staking.rs
+++ contracts/mainchain/src/staking.rs
@@ -1,6 +1,7 @@
 use near_sdk::{env, json_types::U128, near_bindgen, AccountId, Balance, PromiseError, PromiseOrValue};
 
 use crate::{
+    account::HumanReadableAccount,
     fungible_token::{ft, GAS_FOR_FT_ON_TRANSFER},
     MainchainContract,
     MainchainContractExt,
@@ -145,16 +146,18 @@ impl MainchainContract {
     }
 
     pub fn internal_withdraw(&mut self, amount: Balance) {
-        let amount: Balance = amount.into();
         assert!(amount > 0, "Withdrawal amount should be positive");
-
         let account_id = env::predecessor_account_id();
-        let account = self.internal_get_account(&account_id);
+        let mut account = self.internal_get_account(&account_id);
         assert!(account.unstaked >= amount, "Not enough unstaked balance to withdraw");
         assert!(
             account.unstaked_available_epoch_height <= env::epoch_height(),
             "The unstaked balance is not yet available due to unstaking delay"
         );
+
+        // update account
+        account.unstaked -= amount;
+        self.internal_save_account(&account_id, &account);
 
         // transfer the tokens, then validate/update state in `withdraw_callback()`
         ft::ext(self.seda_token.clone())
@@ -277,6 +280,70 @@ impl MainchainContract {
         self.internal_unstake(amount);
     }
 
+    /*************** */
+    /* View methods */
+    /*************** */
+
+    /// Returns the unstaked balance of the given account.
+    pub fn get_account_unstaked_balance(&self, account_id: AccountId) -> U128 {
+        self.get_human_readable_account(account_id).unstaked_balance
+    }
+
+    /// Returns the staked balance of the given account.
+    /// NOTE: This is computed from the amount of "stake" shares the given
+    /// account has and the current amount of total staked balance and total
+    /// stake shares on the account.
+    pub fn get_account_staked_balance(&self, account_id: AccountId) -> U128 {
+        self.get_human_readable_account(account_id).staked_balance
+    }
+
+    /// Returns the total balance of the given account (including staked and
+    /// unstaked balances).
+    pub fn get_account_total_balance(&self, account_id: AccountId) -> U128 {
+        let account = self.get_human_readable_account(account_id);
+        (account.unstaked_balance.0 + account.staked_balance.0).into()
+    }
+
+    /// Returns `true` if the given account can withdraw tokens in the current
+    /// epoch.
+    pub fn is_account_unstaked_balance_available(&self, account_id: AccountId) -> bool {
+        self.get_human_readable_account(account_id).can_withdraw
+    }
+
+    /// Returns the total staking balance.
+    pub fn get_total_staked_balance(&self) -> U128 {
+        self.total_staked_balance.into()
+    }
+
+    /// Returns human readable representation of the account for the given
+    /// account ID.
+    pub fn get_human_readable_account(&self, account_id: AccountId) -> HumanReadableAccount {
+        let account = self.internal_get_account(&account_id);
+        HumanReadableAccount {
+            account_id,
+            unstaked_balance: account.unstaked.into(),
+            staked_balance: self
+                .staked_amount_from_num_shares_rounded_down(account.stake_shares)
+                .into(),
+            can_withdraw: account.unstaked_available_epoch_height <= env::epoch_height(),
+        }
+    }
+
+    /// Returns the number of accounts that have positive balance on this
+    /// staking pool.
+    pub fn get_number_of_accounts(&self) -> u64 {
+        self.accounts.len()
+    }
+
+    /// Returns the list of accounts
+    pub fn get_human_readable_accounts(&self, from_index: u64, limit: u64) -> Vec<HumanReadableAccount> {
+        let keys = self.accounts.keys_as_vector();
+
+        (from_index..std::cmp::min(from_index + limit, keys.len()))
+            .map(|index| self.get_human_readable_account(keys.get(index).unwrap()))
+            .collect()
+    }
+
     #[private] // require caller to be this contract
     pub fn withdraw_callback(
         &mut self,
@@ -284,15 +351,14 @@ impl MainchainContract {
         account_id: AccountId,
         amount: U128,
     ) {
+        let mut account = self.internal_get_account(&account_id);
         if call_result.is_err() {
             env::log_str("withdraw failed");
+            // revert withdrawal
+            account.unstaked += amount.0;
+            self.internal_save_account(&account_id, &account);
             return;
         }
-
-        // update account
-        let mut account = self.internal_get_account(&account_id);
-        account.unstaked -= amount.0;
-        self.internal_save_account(&account_id, &account);
 
         env::log_str(
             format!(
