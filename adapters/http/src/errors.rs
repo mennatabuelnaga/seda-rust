295c84b6c06268b1adbdc13c88cf671e50682156
gluax <16431709+gluax@users.noreply.github.com>
2023-01-13 11:32:07 -0800

    refactor: rm adapters/http

--- adapters/http/src/errors.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error;
-
-#[derive(Error, Debug)]
-pub enum HttpAdapterError {
-    #[error("Rusqlite Error: {0}")]
-    ReqwestError(#[from] reqwest::Error),
-}
-
-pub type Result<T, E = HttpAdapterError> = core::result::Result<T, E>;
